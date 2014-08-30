5eb4498381428da904e68a877da86bfc5722de4f
ranpe <ranpe@users.noreply.github.com>
2014-08-30 09:34:17 +0300

    Update ranpe.py

--- ranpe.py
+++ ranpe.py
@@ -2,15 +2,15 @@ def main():
 	print "Nimesi:"
 	name = raw_input()
 	print "Moi,", name
+	print "Mika on lempivarisi?"
+	name = raw_input()
+	print "Jaa, lempivarisi on siis", name
 	print "Plus-laskin"
 	a = input("Valitse numero:")
 	b = input("Sano toinen numero:")
 	c = a + b
 	print a, "plus", b, "on yhtasuuri kuin ", c
-	print "Mika on lempivarisi?"
-	name = raw_input()
-	print "Jaa, lempivarisi on siis", name
-	print "Rasmuksen eka python ohjelma loppu tahan"
+	print "Ranpen eka python ohjelma loppu tahan."
 	print "(c) Ranpe"
 	return 0
 
