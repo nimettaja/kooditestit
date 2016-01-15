4281f44011cd0cd0dea0bb86f913888d01b81655
nimettaja <nimettaja@gmail.com>
2016-01-15 21:52:26 +0200

    Update testii.py
    muokattu paskaa vähemmän paskaksi

--- testii.py
+++ testii.py
@@ -1,17 +1,20 @@
 # -*- coding: latin-1 -*-
 import getpass 
+
+
 vip = ["Teemu", "Rasmus"]
-nimi = raw_input("Nimesi pls (vain etunimi ja oikeinkirioitus): ")
+nimi = input("Nimesi pls (vain etunimi ja oikeinkirioitus): ")
+
 if nimi in vip:
-	print "ur in"
+	print("ur in")
 else: 
-	print "fuk u"
+	print("fuk u")
+	
+print("Tervetuloa!")
 
-print "Tervetuloa!"
-
-tunnus = raw_input("Tunnus: ")
+tunnus = input("Tunnus: ")
 salasana = getpass.getpass("Salasana: ")
 if tunnus == "aetis" and salasana == "123":
-    print "Welcome, Neo."
+	print "Welcome, Neo."
 else:
 	print "Try again, skrub..."
